8c4b46e54f0afc9d46dab4ca73d955105b25353f
Christoph Suter <christoph.suter@noser.com>
2018-01-14 12:01:36 +0100

    Init Repo

--- src/main/java/ch/noser/kpz/springbootdemo/GreetingController.java
+++ src/main/java/ch/noser/kpz/springbootdemo/GreetingController.java
@@ -1,28 +1,26 @@
 package ch.noser.kpz.springbootdemo;
 
 
-import org.springframework.web.bind.annotation.*;
-
 import java.util.concurrent.atomic.AtomicLong;
 
-@RestController()
+//@RestController()
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
 
-    @RequestMapping(value = "/greeting/{path}", method = {RequestMethod.GET, RequestMethod.HEAD}, produces = "application/json")
-    public Greeting getGreeting(@RequestParam(value="name", defaultValue="World") String name, @PathVariable(value="path") String path, String test) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name + " " + path));
-    }
-
-    @RequestMapping(value = "/greeting/{path}", method = {RequestMethod.POST}, produces = "application/json")
-    public Greeting postGreeting(@RequestParam(value="name", defaultValue="World") String name,
-                                 @PathVariable(value="path") String path,
-                                 @RequestBody String test) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name + " " + path));
-    }
+//    @RequestMapping(value = "/greeting/{path}", method = {RequestMethod.GET, RequestMethod.HEAD}, produces = "application/json")
+//    public Greeting getGreeting(@RequestParam(value="name", defaultValue="World") String name, @PathVariable(value="path") String path, String test) {
+//        return new Greeting(counter.incrementAndGet(),
+//                            String.format(template, name + " " + path));
+//    }
+//
+//    @RequestMapping(value = "/greeting/{path}", method = {RequestMethod.POST}, produces = "application/json")
+//    public Greeting postGreeting(@RequestParam(value="name", defaultValue="World") String name,
+//                                 @PathVariable(value="path") String path,
+//                                 @RequestBody String test) {
+//        return new Greeting(counter.incrementAndGet(),
+//                            String.format(template, name + " " + path));
+//    }
 }
