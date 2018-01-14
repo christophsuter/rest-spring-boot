d17279ad8216ed9cfed3d2aa1b8b78b3bfd66a6f
Christoph Suter <christoph.suter@noser.com>
2018-01-14 11:49:52 +0100

    Initial commit

---
+++ src/test/java/ch/noser/kpz/springbootdemo/DocuTest.java
@@ -0,0 +1,51 @@
+package ch.noser.kpz.springbootdemo;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DocuTest {
+
+    @Rule
+    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/generated-snippets");
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+                                      .apply(documentationConfiguration(this.restDocumentation))
+                                      .build();
+    }
+
+    @Test
+    public void generateDocu() throws Exception {
+        this.mockMvc.perform(get("/hal-greeting").accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andDo(document("index",
+                                    responseFields(fieldWithPath("content").description("content of the payload"),
+                                                   fieldWithPath("_links").description("self link"))));
+    }
+
+}
